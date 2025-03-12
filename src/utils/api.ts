c2bba380712f3c4354d1fdc931591de3391d7b0f
0xyz148 <chaaaamni2@gmail.com>
2025-03-12 11:02:12 +0900

    add devnet api endpoint

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = process.env.STITCH_IS_LOCAL ? 'http://localhost:8080' : '';
+export const BASE_URL = process.env.STITCH_IS_LOCAL
+  ? 'http://localhost:8080'
+  : 'https://api-devnet.stitch-ai.co';
